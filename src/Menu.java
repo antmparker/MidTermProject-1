9bfe94ba86c71d00686108c9bbf36a6ceb18df21
C.Z. Shows <user@Users-MacBook-Pro-3.local>
2017-02-21 11:15:55 -0500

    class changes

--- src/Menu.java
+++ src/Menu.java
@@ -1,12 +1,33 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 /**
  * Created by user on 2/21/17.
  */
 public class Menu {
     //method to findbyArtist
-    public static Album findByArtist (Album artist){
-    return artist;
+    public static Album findByArtist (Scanner scan Album artist) {
+        for (artist a :
+             ) {
+            
+        }
+        if (scan.equalsIgnoreCase(studentNames[i]) ) {
+
+            found = true;
+
+            String foundName = studentNames[i];
+
+            System.out.println("Great! I can tell you " + foundName + "'s favorite food and where "
+                    + foundName + " is from!");
+
+            System.out.println(foundName + " is from " + homeTowns[i] +"!");
+
+            System.out.println("And " + foundName + " loves to eat " + favoriteFoods[i]);
+
+        }
+
+
+        return artist;
     }
 //find by title
     public static Album findByTitle (Album title) {
