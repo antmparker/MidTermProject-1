4bc1eaf3a3c7307d44ebdd1049cb98e314275107
Anthony Parker <anthonymparker1@yahoo.com>
2017-02-21 16:47:20 -0500

    Update Main

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,33 @@
+import java.util.Scanner;
 
- /* Created by user on 2/21/17.
+/* Created by user on 2/21/17.
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello");
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Welcome to the Grand Circus CD Rental");
+        System.out.println();
+        System.out.println("How do you want to search for CD?");
+        System.out.print("Enter (1) for artist search. Enter (2) for title keyword. Enter (3) for list: ");
+
+        int search  = scan.nextInt();
+Inventory inv = new Inventory();
+        if (search == 1) {
+            System.out.print("Enter artist name: ");
+                inv.findByArtist(scan);
+
+        }else if (search == 2) {
+
+            System.out.println("Enter title keyword: ");
+            inv.findByTitle(scan);
+
+        }else{
+            System.out.println(inv.getAlbumInv());
+        }
+
+
     }
 }
 
