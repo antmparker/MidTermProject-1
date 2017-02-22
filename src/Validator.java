49a79841769e30a12c0851891147a418e9272915
Sharron J <sj7723@aol.com>
2017-02-22 18:55:32 -0500

    rmvd Scanner scan = scnr(sys.in) bcz it wasl alrdy being passed in the method, it was duplicated

--- src/Validator.java
+++ src/Validator.java
@@ -4,32 +4,32 @@ import java.util.Scanner;
  * Created by Sharron 2/20/17
  */
 public class Validator {
-    Scanner scan = new Scanner(System.in);
 
 
-    public static String getString(Scanner sc, String prompt) {
-        System.out.print(prompt);
-        String s = sc.next();  // read user entry
+    public static String getString(Scanner sc, String prompt) {//should only return a string
+        System.out.print(prompt);//this displays to the counsel the message from user input
+        String s = sc.next();  // read user entry//turns everything into a string
         sc.nextLine();  // discard any other data entered on the line
-        return s;
+        return s;//returning a string that came from the scanner input
     }
 
-    public static int getInt(Scanner sc, String prompt) {
-        int i = 0;
-        boolean isValid = false;
-        while (isValid == false)
+    public static int getInt(Scanner sc, String prompt) {//should return an integer only
+        int i = 0;//interger is set to zero
+        boolean isValid = false;//initializing a beginning value to false
+        while (isValid == false)//while loop repeats the code inside the loop until it returns isValid to true
         {
             System.out.print(prompt);
-            if (sc.hasNextInt()) {//grabs another line of input
-                i = sc.nextInt();
-                isValid = true;
+            if (sc.hasNextInt()) {//looks to see if there's a next integer to get/runs once
+                i = sc.nextInt();//scanner grabs the next integer
+                isValid = true;//set this to a different value if integer is found
                 
             }
             else {
-                System.out.println("Invalid integer value. Try again.");
+                System.out.println("Invalid integer value. Try again.");//if no int found message to user displayed
+                //tell user to go back through the loop & try it again
             }
             sc.nextLine();// discard any other data entered on the line
-        }
+        }//this goes back to the top of while loop to run the text again
         return i;
     }
 
@@ -67,7 +67,7 @@ public class Validator {
         return d;
     }
 
-    public static double getDouble(Scanner sc, String prompt, double min, double max) {
+    public static double getDouble(Scanner sc, String prompt, double min, double max) {//this one checks double & in range
         double d = 0;
         boolean isValid = false;
         while (isValid == false) {
